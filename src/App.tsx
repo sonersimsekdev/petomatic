12c91e66a239aec09fad72c27b0a5ddaba5281f7
Soner Şimşek <sonersimsekhd@gmail.com>
2022-05-09 09:02:11 +0300

    project is  created

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  theme,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  SimpleGrid,
+  WrapItem,
+  Center,
+  Wrap,
+  Container,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Deneme } from "./Deneme"
+import { AirbnbExample } from "./AirbnbExample"
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+       
 
-export default App;
+        <Container><Center> <Wrap>
+          <WrapItem>
+            <Center w='180px' h='80px' bg='red.200'>
+              Box 1
+            </Center>
+          </WrapItem>
+          <WrapItem>
+            <Center w='180px' h='80px' bg='green.200'>
+              Box 2
+            </Center>
+          </WrapItem>
+          <WrapItem>
+            <Center w='180px' h='80px' bg='tomato'>
+              Box 3
+            </Center>
+          </WrapItem>
+          <WrapItem>
+            <Center w='180px' h='80px' bg='blue.200'>
+              Box 4
+            </Center>
+          </WrapItem>
+        </Wrap></Center></Container>
+
+        <Container><Center></Center></Container>
+
+
+        <Container><Center><Grid templateColumns='repeat(5, 1fr)' gap={6}>
+          <GridItem w='100%' h='10' bg='black.500' />
+          <GridItem w='100%' h='10' bg='black.500' />
+          <GridItem w='100%' h='10' bg='black.500' />
+          <GridItem w='100%' h='10' bg='black.500' />
+          <GridItem w='100%' h='10' bg='black.500' />
+        </Grid></Center></Container>
+
+        <Container>
+          <Center><Wrap >
+
+            <WrapItem> <Box> <AirbnbExample /></Box></WrapItem>
+            <WrapItem> <Box> <AirbnbExample /></Box></WrapItem>
+            <WrapItem> <Box> <AirbnbExample /></Box></WrapItem>
+            <WrapItem> <Box> <AirbnbExample /></Box></WrapItem>
+            <WrapItem> <Box> <AirbnbExample /></Box></WrapItem>
+            <WrapItem> <Box> <AirbnbExample /></Box></WrapItem>
+            <WrapItem> <Box> <AirbnbExample /></Box></WrapItem>
+            <WrapItem> <Box> <AirbnbExample /></Box></WrapItem>
+            <WrapItem> <Box> <AirbnbExample /></Box></WrapItem>
+            <WrapItem> <Box> <AirbnbExample /></Box></WrapItem>
+
+
+          </Wrap></Center>
+        </Container>
+
+
+
+
+
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
---
+++ src/Deneme.tsx
@@ -0,0 +1,64 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react"
+
+export const Deneme = () => (
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <TableContainer>
+  <Table variant='simple'>
+    <TableCaption>Imperial to metric conversion factors</TableCaption>
+    <Thead>
+      <Tr>
+        <Th>To convert</Th>
+        <Th>into</Th>
+        <Th isNumeric>multiply by</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      <Tr>
+        <Td>inches</Td>
+        <Td>millimetres (mm)</Td>
+        <Td isNumeric>25.4</Td>
+      </Tr>
+      <Tr>
+        <Td>feet</Td>
+        <Td>centimetres (cm)</Td>
+        <Td isNumeric>30.48</Td>
+      </Tr>
+      <Tr>
+        <Td>yards</Td>
+        <Td>metres (m)</Td>
+        <Td isNumeric>0.91444</Td>
+      </Tr>
+    </Tbody>
+    <Tfoot>
+      <Tr>
+        <Th>To convert</Th>
+        <Th>into</Th>
+        <Th isNumeric>multiply by</Th>
+      </Tr>
+    </Tfoot>
+  </Table>
+</TableContainer>
+      </Grid>
+    </Box>
+
+)
